0b6a6732428cd0de5d9af607857a1dc7b7540191
PercussiveElbow <PercussiveElbow@users.noreply.github.com>
2020-10-29 00:00:51 +0000

    Fixes for a few of the orgs

--- sites/infosec_institute.py
+++ sites/infosec_institute.py
@@ -7,8 +7,11 @@ infosec_insitute_base = "https://www.infosecinstitute.com/white-papers/"
 def infosec_insitute():
     print("Scraping InfoSecInstitute")
     pageSoup = BeautifulSoup(requests.get(infosec_insitute_base,headers=HEADERS).text,"lxml")
-    docs = pageSoup.findAll("a",{"class": "download-document"})
+    docs = pageSoup.findAll("a",{"class": "infosec-library-card-wrapper"})
     for doc in docs:
-        download_url = doc["href"]
-        file_download(download_url,"Whitepapers/InfoSecInstitute/" +doc.find("h4").text[1:] ,download_url,doc.find("p").text)
+        download_page_url = doc["href"] + "?utm_status=success"  # z0mg leet 0 day bypass no addy required ;OOOO  
+        downloadPageSoup = BeautifulSoup(requests.get(download_page_url,headers=HEADERS).text,"lxml")
+        print(download_page_url)
+        download_url = downloadPageSoup.find("div", {"class": "infosec-form-success-button-wrapper"}).find("a")["href"]
+        file_download(download_url,"Whitepapers/InfoSecInstitute/" +doc.find("h3").text[1:] ,download_url,"")
     print("Finished InfoSecInstitute")
--- sites/symantec.py
+++ sites/symantec.py
@@ -2,14 +2,15 @@ from bs4 import BeautifulSoup
 import requests,os,time
 from sites.helper.help_utils import *
 
-symantec_base = "https://www.symantec.com/"
+symantec_base = "https://www.broadcom.com/support/security-center/publications/archive"
 
 def symantec():
-    print("Scraping Symantec")
-    pageSoup = BeautifulSoup(requests.get(symantec_base + "security-center/white-papers",headers=HEADERS).text,"lxml")
-    whitepapers = pageSoup.find_all("span",{"class": "symantecBlueBullet"})
+    print("Scraping Broadcom")
+    pageSoup = BeautifulSoup(requests.get(symantec_base,headers=HEADERS).text,"lxml")
+    whitepapers = pageSoup.find_all("div",{"class": "promo"})
     for whitepaper in whitepapers:
+        print(whitepaper)
         download_url = whitepaper.find("a")["href"]
         download_title = whitepaper.find("a").text
-        file_download(symantec_base + download_url,"Whitepapers/Symantec/"+ download_title, download_url,"")
-    print("Finished Symantec")
+        file_download(download_url,"Whitepapers/Broadcom/"+ download_title, download_url,"")
+    print("Finished Broadcom")
